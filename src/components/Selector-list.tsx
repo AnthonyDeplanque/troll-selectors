e432748a4cf288f63768ed66c22b823475f87a1c
anthony deplanque <anthony.deplanque@csgroup.eu>
2024-03-11 16:26:33 +0100

    wip

--- src/components/Selector-list.tsx
+++ src/components/Selector-list.tsx
@@ -1,60 +1,38 @@
-import { useEffect, useState } from "react";
-import Selector from "./Selector";
-
+import { useEffect, useState } from 'react';
+import Selector from './Selector';
+import { createArrayOfRandomBooleans, setToggle } from './selector-helpers';
 
 const SelectorList = () => {
+    const numberOfSelectors = 10;
+    const [values, setValues] = useState<boolean[]>([]);
 
+    useEffect(() => {
+        setValues(createArrayOfRandomBooleans(numberOfSelectors, 4));
+    }, []);
 
-  const createArrayOfBooleans = (length: number) => Array.from({ length: length }).map(() => Math.floor(Math.random() * 100) % 2 === 0);
-
-  const numberOfSelectors = 10;
-  const [values, setValues] = useState<boolean[]>([]);
-
-  useEffect(() => {
-    setValues(createArrayOfBooleans(numberOfSelectors))
-  }, [])
-
-  const setToggle = (previousIndex: number) => {
-    const numberOfIndexToChange = Math.floor(Math.random() * 2) + 2;
-    const indexesToChange = Array.from({ length: numberOfIndexToChange }).map(() => {
-      let indexToChange: number;
-      do {
-        indexToChange = Math.floor(Math.random() * values.length)
-      }
-      while (indexToChange === previousIndex);
-      return indexToChange
-    });
-    indexesToChange.push(previousIndex);
-    setValues((previousValues) => {
-      const newValuesArray = [...previousValues];
-      indexesToChange.forEach((indexToChange) => newValuesArray[indexToChange] = !newValuesArray[indexToChange])
-      return newValuesArray
-    })
-    if (values.some(() => true)) {
-      const indexToSetToTrue = Math.floor(Math.random() * numberOfSelectors)
-      setValues((previousValues) => {
-        const newValuesArray = [...previousValues]
-        newValuesArray[indexToSetToTrue] = true;
-        return newValuesArray
-      })
-    }
-
-
-  }
-
-
-  return <div>
-    {
-      values.length ?
-        values.map((value, index) => {
-          return <Selector key={`selector-${index}`}
-            toggle={value}
-            label={`bug ${105 + index}`}
-            setToggle={() => setToggle(index)} />
-        })
-        : ""
-    }
-  </div>
-}
+    return (
+        <div>
+            {values.length
+                ? values.map((value, index) => {
+                      return (
+                          <Selector
+                              key={`selector-${index}`}
+                              toggle={value}
+                              label={`bug ${105 + index}`}
+                              setToggle={() =>
+                                  setToggle(
+                                      index,
+                                      numberOfSelectors,
+                                      values,
+                                      setValues,
+                                  )
+                              }
+                          />
+                      );
+                  })
+                : ''}
+        </div>
+    );
+};
 
 export default SelectorList;
---
+++ src/components/selector-helpers.ts
@@ -0,0 +1,52 @@
+export const createArrayOfRandomBooleans = (length: number, trollLimit: number) => {
+    const array = Array.from({ length: length }).map(() => false);
+    const indexesToChange = Array.from({
+        length: Math.floor(Math.random() * trollLimit) + 1,
+    }).map(() => Math.floor(Math.random() * length) + 1);
+    indexesToChange.forEach((index) => (array[index] = true));
+    return array;
+};
+
+export const setToggle = (
+    switchedToggleIndex: number,
+    numberOfSelectors: number,
+    values: boolean[],
+    setValues: (value: React.SetStateAction<boolean[]>) => void,
+) => {
+    console.log(switchedToggleIndex, numberOfSelectors, values);
+
+    // get values array indexes that can be changed (not the selected one and not a "false" value)
+    const indexThatCanBeChanged: number[] = values
+        .filter((_, index: number) => index !== switchedToggleIndex)
+        .filter((value: boolean) => value === false)
+        .map((_, index: number) => index);
+
+    // Determine how many indexes are to change
+    const indexesLength = Math.floor(Math.random() * 2) + 1;
+
+    // Change randomly values
+    while (indexThatCanBeChanged.length !== indexesLength || indexesLength > indexThatCanBeChanged.length) {
+        const arrayIndexToSplice = Math.floor(Math.random() * (indexThatCanBeChanged.length + 1));
+        indexThatCanBeChanged.splice(arrayIndexToSplice, 1);
+    }
+
+    // set new values
+    setValues((previousValues) => {
+        const newValuesArray = [...previousValues];
+        indexThatCanBeChanged.forEach((index) => (newValuesArray[index] = !newValuesArray[index]));
+        newValuesArray[switchedToggleIndex] = !newValuesArray[switchedToggleIndex];
+        return newValuesArray;
+    });
+
+    // If all is false, game is over, we don't want that so we'll change randomly one value index
+    if (!values.some(() => true)) {
+        let indexToSetToTrue:number 
+        do {
+            indexToSetToTrue = Math.floor(Math.random() * numberOfSelectors)+1;
+        }while (indexToSetToTrue === switchedToggleIndex )
+        setValues((previousValues) => {
+            const newValuesArray = [...previousValues];
+            newValuesArray[indexToSetToTrue] = true;
+            return newValuesArray;
+        });
+};
